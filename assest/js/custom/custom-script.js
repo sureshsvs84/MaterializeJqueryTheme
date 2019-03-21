a8669318d3b6c100a077f83d9ccdfd23a0ee59c7
sureshsvs84 <suresh.s@smnetserv.com>
2019-03-22 02:26:05 +0530

    Ui updated

--- assest/js/custom/custom-script.js
+++ assest/js/custom/custom-script.js
@@ -1,11 +1,24 @@
-/*================================================================================
-	Item Name: Materialize - Material Design Admin Template
-	Version: 5.0
-	Author: PIXINVENT
-	Author URL: https://themeforest.net/user/pixinvent/portfolio
-================================================================================
 
-NOTE:
-------
-PLACE HERE YOUR OWN JS CODES AND IF NEEDED.
-WE WILL RELEASE FUTURE UPDATES SO IN ORDER TO NOT OVERWRITE YOUR CUSTOM SCRIPT IT'S BETTER LIKE THIS. */
+    
+$(function() {
+   "use strict";
+$(document).ready(function(){
+    var newHeight = $(".main-full").height();
+    $(".cardSearchPortal").css('min-height', (newHeight-120));
+    
+    $("#floating-btn-search").click(function(){
+      $( ".cardSearchPortal" ).find('.searchPortal').removeClass('mt-10');
+      $( ".cardSearchPortal" ).find('.searchPortal').addClass('mt-1');
+      $( ".cardSearchPortal" ).find('.searchResult').removeClass('hide').addClass('show');
+ });
+    
+    
+    
+   
+});
+ $(window).resize(function() {
+			 $(".cardSearchPortal").css('min-height', (newHeight-120));
+		});   
+ 
+    
+});
